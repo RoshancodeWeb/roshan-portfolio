56cffb5aba2b6557b2a0ecbb373bbf2b5723e433
RoshancodeWeb <roshanail7613@gmail.com>
2024-09-21 22:34:15 -0700

    changed icons

--- src/app/components/TechStack.tsx
+++ src/app/components/TechStack.tsx
@@ -8,18 +8,16 @@ const TechStack = () => {
             <p className='w-fit h-fit relative left-[50%] -translate-x-[50%] lg:translate-x-0 lg:left-0 font-bold text-xl pb-1  border-b-2 lg:border-b-0 lg:pr-2 lg:border-r-2 border-[#939395] '>Tech Stack</p>
             <div className='stack w-full h-fit lg:w-fit md:flex '>
                <div className='w-full h-fit flex px-10 justify-between pt-5 lg:pt-0 gap-2'>
-                  <Image width={100} height={100} className='lg:w-[150px] cursor-pointer relative  transition-transform ease-in-out duration-700 hover:-translate-y-8'  src="https://skillicons.dev/icons?i=html,css" alt="img1"  />
-                  <Image width={100} height={100} className='lg:w-[150px] cursor-pointer relative transition-transform ease-in-out duration-700 hover:-translate-y-8'    src=" https://skillicons.dev/icons?i=js,ts" alt="img2"  />
+                  <Image width={100} height={100} className='lg:w-[150px] cursor-pointer relative  transition-transform ease-in-out duration-700 hover:-translate-y-8'  src="/icon/icon-one.svg" alt="img1"  />
+                  <Image width={100} height={100} className='lg:w-[150px] cursor-pointer relative transition-transform ease-in-out duration-700 hover:-translate-y-8'    src="/icon/icon-two.svg" alt="img2"  />
                </div>
                <div className='w-full h-fit flex px-10 justify-between pt-5 lg:pt-0 gap-2'>
-                  <Image width={100} height={100} className='lg:w-[150px] cursor-pointer transition-transform ease-in-out duration-700 hover:-translate-y-8'  src="https://skillicons.dev/icons?i=react,next" alt="img3"  />
-                  <Image width={100} height={100}  className='lg:w-[150px] cursor-pointer transition-transform ease-in-out duration-700 hover:-translate-y-8'  src="https://skillicons.dev/icons?i=mongo,express" alt="img4"  />
+                  <Image width={100} height={100} className='lg:w-[150px] cursor-pointer transition-transform ease-in-out duration-700 hover:-translate-y-8'  src="/icon/icon-three.svg" alt="img3"  />
+                  <Image width={100} height={100}  className='lg:w-[150px] cursor-pointer transition-transform ease-in-out duration-700 hover:-translate-y-8'  src="/icon/icon-four.svg" alt="img4"  />
                </div>
                <div className='w-full h-fit flex   justify-center pt-5 lg:pt-0 '>
-                  <Image width={100} height={100} className='lg:w-[110px] cursor-pointer transition-transform ease-in-out duration-700 hover:-translate-y-8'  src="https://skillicons.dev/icons?i=nodejs,tailwind" alt="img5"  />  
+                  <Image width={100} height={100} className='lg:w-[110px] cursor-pointer transition-transform ease-in-out duration-700 hover:-translate-y-8'  src="/icon/icon-five.svg" alt="img5"  />  
                </div>
-
-               
             </div>
 
          </div>
